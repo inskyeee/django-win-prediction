f261378fab3b12aed1f736f029faaf8d2e5b6ba3
Dmitrii <dmitrylor@gmail.com>
2024-04-25 11:07:14 +0100

    Test the model performance

---
+++ Model/test-model.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score
+import joblib
+
+
+loaded_model = joblib.load('Model/model.pkl')
+loaded_scaler = joblib.load('Model/scaler.pkl')
+
+data = pd.read_csv('Model/data.csv', sep=';')
+data_scaled = loaded_scaler.transform(data.drop('Target', axis=1))
+
+predictions = loaded_model.predict(data_scaled)
+
+
+print(accuracy_score(data['Target'], predictions))
