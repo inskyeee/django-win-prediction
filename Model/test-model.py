937e6d0ed40997ac504f701e675bb913b3dc58be
Dmitrii <dmitrylor@gmail.com>
2024-05-07 11:52:44 +0100

    set it up for new datafiles

--- Model/test-model.py
+++ Model/test-model.py
@@ -6,7 +6,8 @@ pd.set_option('display.max_columns', None)
 loaded_model = joblib.load('Model/model.pkl')
 loaded_scaler = joblib.load('Model/scaler.pkl')
 
-data = pd.read_csv('Model/data.csv')
+data = pd.read_csv('Model/dota2Test.csv')
+data.columns = ['Target'] + ['Game mode'] + ['Game type'] + [f'Feature_{i}' for i in range(1, data.shape[1] - 2)]
 data_scaled = loaded_scaler.transform(data.drop('Target', axis=1))
 
 predictions = loaded_model.predict(data_scaled)
@@ -15,7 +16,8 @@ accuracy = accuracy_score(data['Target'], predictions)
 coefficients = loaded_model.coef_[0]
 features = loaded_scaler.feature_names_in_
 
-feature_importance = pd.Series(coefficients, index=features).sort_values(ascending=False)
+feature_importance = pd.Series(coefficients, index=features)
+feature_importance.sort_values(key=lambda x: abs(coefficients), ascending=False, inplace=True)
 
 # print(feature_importance)
 # print(accuracy)
