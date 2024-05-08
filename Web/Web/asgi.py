60900ead221686799594c344ff7f2216d8859bab
Dmitrii <dmitrylor@gmail.com>
2024-05-08 11:18:53 +0100

    Updated the settings for new names

--- Web/Web/asgi.py
+++ Web/Web/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RecipeHelper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web.settings')
 
 application = get_asgi_application()
--- Web/Web/wsgi.py
+++ Web/Web/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RecipeHelper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Web.settings')
 
 application = get_wsgi_application()
