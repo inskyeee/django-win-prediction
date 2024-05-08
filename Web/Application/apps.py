87cc126910c7d62599955c87e2b623b5d5d2fc77
Dmitrii <dmitrylor@gmail.com>
2024-05-08 11:21:00 +0100

    Updated apps.py

--- Web/Application/apps.py
+++ Web/Application/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AihelperConfig(AppConfig):
+class ApplicationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AIhelper'
+    name = 'Application'
