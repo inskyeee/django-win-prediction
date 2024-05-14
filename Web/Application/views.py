8a755ff268e75d42d5895f0697fdacfdef638503
Dmitrii <dmitrylor@gmail.com>
2024-05-14 10:33:41 +0100

    Added predictions on pages

--- Web/Application/views.py
+++ Web/Application/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
 from .forms import GameForm
+from .prediction import process_game
 
 def index(request):
     if request.method == 'POST':
         form = GameForm(request.POST)
         if form.is_valid():
-            game = form.save()
+            game = form.cleaned_data
+            win_chance = process_game(game)
+            request.session['chance_of_win'] = win_chance[1]
+            form.save()
             return redirect('prediction')  
 
     else:
@@ -14,4 +18,6 @@ def index(request):
     return render(request, 'index.html', {'form': form})
     
 def prediction(request):
-    return render(request, 'prediction.html')
+    chance_of_win = request.session.get('chance_of_win')
+    win = chance_of_win > 0.5
+    return render(request, 'prediction.html', {'chance_of_win': chance_of_win, 'win': win})
